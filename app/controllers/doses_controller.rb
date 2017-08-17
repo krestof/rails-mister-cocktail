f3b43875688d3811731a648ca59dced33f432e73
Kristofer Espinosa <kristofer.espinosa@gmail.com>
2017-08-17 17:24:02 +0200

    rake working

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,25 +1,36 @@
 class DosesController < ActionController::Base
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredients = Ingredient.all
     @dose = Dose.new
+    # @cocktail = Cocktail.find(params[:cocktail_id])
+    # # @ingredients = Ingredient.all
+    # @dose = Dose.new({cocktail_id: @cocktail.id})
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @ingredients = Ingredient.all
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    @dose.save
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
   end
 
   def destroy
-    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(params[:cocktail_id])
+    redirect_to cocktail_path
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
